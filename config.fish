1ed85f4e65a061911d3232cb4fdf9bd72784f870
3inar <einar.j.holsbo@uit.no>
2025-12-09 10:50:06 +0100

    switch to syncthing

--- config.fish
+++ config.fish
@@ -5,6 +5,6 @@ if status is-interactive
     neofetch
     # Commands to run in interactive sessions can go here
 
-    alias wlog="cd ~/Dropbox/knowledge; nvim log.txt"
-    alias website="cd ~/Dropbox/knowledge/website/notes; nvim index.md"
+    alias wlog="cd ~/Sync/knowledge; nvim log.txt"
+    alias website="cd ~/Sync/knowledge/website/notes; nvim index.md"
 end
